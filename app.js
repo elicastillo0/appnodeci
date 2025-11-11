3326ac759531079bf983afe539a61ecc0870ccff
elicastillo0 <elicastillo92@yahoo.com>
2025-11-11 14:04:56 +0100

    Corrijo app.js y verifico estructura para Heroku

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Ruta principal
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello, World!'
-    })
-})
+  res.status(200).json({
+    message: 'Hello, World!'
+  });
+});
 
 // Activando workflow tras regenerar dependencias
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
-
+  console.log(`Server is running on http://localhost:${port}`);
+});
